fa21d536475a2d2698abcc937c7a2936a68498ca
Tanishq Abraham <tmabraham@ucdavis.edu>
2022-04-25 21:59:54 +0000

    dataset pipeline for codet5 fine-tuning

--- codet5_finetune.py
+++ codet5_finetune.py
@@ -1,6 +1,10 @@
 from transformers import RobertaTokenizer, T5ForConditionalGeneration, AdamW, get_linear_schedule_with_warmup
+from datasets import Dataset
 import torch
+import pandas as pd
 from torch.utils.data import DataLoader
+from functools import partial
+from data.helper import load_json_file
 
 tokenizer = RobertaTokenizer.from_pretrained("Salesforce/codet5-base")
 
@@ -26,4 +30,16 @@ def preprocess_examples(examples, tokenizer, max_input_length, max_target_length
 
     return model_inputs
 
-if __name__ == "__main__": print('hello')
+if __name__ == "__main__":
+    dataset = load_json_file("dataset/CodeReviewSE_clean_QA.json")
+    dataset = Dataset.from_pandas(pd.DataFrame(data=dataset))
+    dataset = dataset.map(partial(preprocess_examples, tokenizer=tokenizer, max_input_length=max_input_length, max_target_length=max_target_length), batched=True, num_proc=16)
+    print(dataset)
+    
+    dataset.set_format(type="torch", columns=['input_ids', 'attention_mask', 'labels'])
+    train_dataloader = DataLoader(dataset, shuffle=True, batch_size=8)
+    batch = next(iter(train_dataloader))
+    print(batch.keys())
+    print(tokenizer.decode(batch['input_ids'][0]))
+    labels = batch['labels'][0]
+    print(tokenizer.decode([label for label in labels if label != -100]))
