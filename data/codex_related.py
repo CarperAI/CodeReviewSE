7ff5d536a71f003ac7c73baf46f25cc5083dc60a
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2022-08-13 01:06:42 +0530

    Final codex_related code

--- data/codex_related.py
+++ data/codex_related.py
@@ -3,6 +3,9 @@ import logging
 from tqdm import tqdm
 import bs4
 from pprint import pprint
+import os
+import openai
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,7 +20,11 @@ def write_dict_to_json(file_path:str,data:dict):
     with open(file_path,"w") as f:
         json.dump(data,f,indent=2)
 
-
+keys_dict = load_json_to_dict("configs/keys.json")
+openai.organization = keys_dict["org"]
+openai.api_key = keys_dict["key"]
+#write_dict_to_json("configs/model_list.json",openai.Model.list())
+logger.info("successfully loaded API key...")
 
 def clean_parsed_code(code_block:str):
     code_block = code_block.replace("<code>","").replace("</code>","")
@@ -40,13 +47,10 @@ class SEData:
         self.dataset : dict = load_json_to_dict(dataset_path)
         self.python_tags : list[str] = ["python", "python-2.x", "python-3.x","py"]
         logger.info(f"Sucessfully loaded dataset to memory...")
-        from pprint import pprint
-        pprint(self.dataset[list(self.dataset.keys())[100]])
         self.data_len = len(self.dataset) #Length of the dataset
-
         self.instruction_template : dict = {
             "type_inference"  : [
-                "For all the identifiers, infer and annotate the type of the identifier"
+                "Annotate type for identifiers in the python code"
             ],
             "test_case_assertion" : [
                 ""
@@ -75,20 +79,57 @@ class SEData:
             
             answer_body = accepted_answer["body"] #Get the body of the accepted answer
             accepted_answer_code : list[str] = parse_html_block_for_code(answer_body)
-            
             return accepted_answer_code
+        else:
+            return []
 
-    def apply_codex_for_type_inference(self,datapoint:str)->dict:
-        pass
+    def apply_codex_for_type_inference(self,input_prompt:str)->dict:
+        """
+        Apply codex for type annotation of the input prompt
 
-    def iter_apply_process(self,length:int):
+        args:
+            input_prompt (str) : input prompt to be annotated
+        """
+        response = openai.Edit.create(
+        model = "code-davinci-edit-001",
+        input = input_prompt,
+        instruction = self.instruction_template["type_inference"][0],
+        temperature=0.7,
+        top_p=1
+        )
+        return response
+
+    def iter_apply_process(self,length:int,if_apply_codex_flag:str="type_inference",hash_path:str="dataset/CodeReviewSE_hash.txt"):
         processed_dict = {}
+        counter = 0
         for i in tqdm(range(length)):
             key = list(self.dataset.keys())[i]
             datapoint = self.dataset[key]
             if key not in processed_dict:
-                processed_dict[key] = {"code":[]}
-            processed_dict[key]["code"] = self.process_datapoint(datapoint)
+                processed_list = self.process_datapoint(datapoint)
+                if len(processed_list) > 0:
+                    processed_dict[key] = {"code":[],"augmented_code":[]}
+                    processed_dict[key]["code"] = processed_list
+                    if if_apply_codex_flag == "type_inference":
+                        for processed_code in processed_list:
+                            try:
+                                hash_text_file = open(hash_path,"a")
+
+                                response = self.apply_codex_for_type_inference(processed_code)
+                                processed_dict[key]["augmented_code"].append(response["choices"][0]["text"])
+                                hash_text_file.write(f'{key}#@#{response["choices"][0]["text"]}#@#{processed_code}\n@##@##@')
+
+                            except openai.error.RateLimitError:
+                                logger.info("Rate limit error. Waiting for 65 seconds...")
+                                time.sleep(65)
+                                hash_text_file.close()
+                                continue
+                            except openai.error.InvalidRequestError:
+                                logger.info("OpenAI error.")
+                                pass
+                    counter += len(processed_list)
+        logger.info(f"Sucessfully processed dataset to python dataset...")
+        logger.info(f"Length of processed code blocks dataset: {counter}")
         return processed_dict
 
 
@@ -106,7 +147,9 @@ class SEData:
         write_dict_to_json("dataset/CodeReviewSE_py.json",self.python_dataset)
 
 if __name__ == "__main__":
+    pass
     se_data = SEData("dataset/CodeReviewSE_py.json")#.iter_filter_python_dataset()
-    pprint(se_data.iter_apply_process(5))
+    processed_python_dataset = se_data.iter_apply_process(100,"type_inference")
+    write_dict_to_json(f"dataset/CodeReviewSE_py_processed_codex_100.json",processed_python_dataset)
     # test_string = "<p>I used a slightly simpler method, but essentially did the same thing:</p>\n\n<pre><code>total = 0\n\nfor n in range(3,1000):\n    if n % 3 == 0:\n        total += n\n    elif n % 5 == 0:\n        total += n\n\n\nprint total\n</code></pre>\n\n<p>The <code>elif</code> makes sure that you only count any factor/divisor once.</p>\n"
     # print(parse_html_block_for_code(test_string))
